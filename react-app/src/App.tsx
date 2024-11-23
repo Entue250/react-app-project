dd5c52c4ed3c380c30d72ea9192bef95c2d26321
Entue250 <edouardoniyomugabo@gmail.com>
2024-11-23 19:59:01 +0200

    Delete the Data

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -187,10 +187,34 @@ function App() {
     fetchUsers();
     //.then((res) => setUsers(res.data));
   }, []);
+
+  const deleteUser = (user: User) => {
+    const originalUsers = [...users];
+    setUsers(users.filter((u) => u.id !== user.id));
+
+    axios
+      .delete("https://jsonplaceholder.typicode.com/users/" + user.id)
+      .catch((err) => {
+        setError(err.message);
+        setUsers(originalUsers);
+      });
+  };
+
   return (
-    <ul>
+    <ul className="list-group">
       {users.map((user) => (
-        <li key={user.id}>{user.name}</li>
+        <li
+          key={user.id}
+          className="list-group-item d-flex justify-content-between"
+        >
+          {user.name}
+          <button
+            className="btn btn-outline-danger"
+            onClick={() => deleteUser(user)}
+          >
+            Delete
+          </button>
+        </li>
       ))}
     </ul>
   );
