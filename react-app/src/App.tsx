35ecd8a1a3ebdb7631add2a5a56cfc87020bd532
Entue250 <edouardoniyomugabo@gmail.com>
2024-11-24 17:59:44 +0200

    Use Custome Hook

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -163,29 +163,10 @@
 import { CanceledError } from "./services/api-client";
 import { useEffect, useState } from "react";
 import userService, { User } from "./services/user-service";
+import useUsers from "./hooks/useUsers";
 
 function App() {
-  const [users, setUsers] = useState<User[]>([]);
-  const [error, setError] = useState("");
-  const [isLoading, setLoading] = useState(false);
-
-  useEffect(() => {
-    // get -> await promise -> res / err
-
-    setLoading(true);
-    const { request, cancel } = userService.getAll<User>();
-    request
-      .then((res) => {
-        setUsers(res.data);
-        setLoading(false);
-      })
-      .catch((err) => {
-        if (err instanceof CanceledError) return;
-        setError(err.message);
-        setLoading(false);
-      });
-    return () => cancel();
-  }, []);
+  const { users, error, isLoading, setUsers, setError } = useUsers();
 
   const deleteUser = (user: User) => {
     const originalUsers = [...users];
